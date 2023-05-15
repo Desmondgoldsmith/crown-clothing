72fbfebe9d0b052cf491de08e42381aaa3a98982
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-15 03:38:45 +0000

    Crown Clothing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import Directory from './components/Directory/Directory';
-import './App.css'
-const categories = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-];
+import {Fragment} from 'react'
+import Home from "./Routes/home/home.component"
+import {createBrowserRouter,createRoutesFromElements,Outlet,RouterProvider, Route} from 'react-router-dom'
 
-const App = () => {
-  return <Directory categories = {categories} />
+
+const Navbar = () => {
+  return(
+    <div>
+      <div>
+        <h1>Navbar</h1>
+      </div>
+      <Outlet/>
+    </div>
+  )
 }
 
-export default App;
+
+const App = () => {
+  const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Fragment>
+    <Route path = "/" element = {<Navbar/>}>
+       <Route index element = {<Home/>}/>
+       <Route path = '/about' element = {<h1>About Us new!</h1>}/>
+    </Route>
+
+    {/* <Route path = "/about" element = {<Navbar/>}>
+       <Route index element = {<h1>About Us</h1>}/>
+    </Route> */}
+    </Fragment>
+  )
+)
+  return(<RouterProvider router = {router}/>)
+}
+
+export default App
+
