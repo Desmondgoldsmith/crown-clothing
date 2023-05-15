b1600cd4bfc88a0cf646deb6adfe247db1a79fce
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-15 13:42:14 +0000

    Experimeting with react router dom v5.4 +

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import Navigation from './Routes/Navigation/Navigation.component'
 const App = () => {
   const router = createBrowserRouter(
   createRoutesFromElements(
+    <>
     <Route path = "/" element = {<Navigation/>}>
        <Route index element = {<Home/>}/>
-       <Route path = '/about' element = {<h1>About Us new!</h1>}/>
+       <Route path = '/shop' element = {<h1>Shop page!</h1>}/>
+       <Route path = '/sign-in' element = {<h1>Sign in!</h1>}/>
     </Route>
+    </>
   )
 )
   return(<RouterProvider router = {router}/>)
--- src/Routes/Navigation/Navigation.component.jsx
+++ src/Routes/Navigation/Navigation.component.jsx
@@ -1,15 +1,18 @@
 import {Fragment} from 'react'
 import {Outlet,Link} from 'react-router-dom'
+import {ReactComponent as CrownLogo} from '../../Asset/crown.svg'
+import './Navigation.styles.scss'
 
 const Navigation = () => {
     return(
         <Fragment>
-        <div>
-            <h1>Navbar</h1>
-            <div>
-            <Link to = '/about'>
-            <button>About page</button>
+        <div className = "navigation">
+            <Link to = '/'>
+            <div className = "logo-container"><CrownLogo/></div>
             </Link>
+            <div className = "nav-links-container">
+            <Link to = "/shop" className = "nav-link">SHOP</Link>
+            <Link to = "/sign-in" className = "nav-link">SIGN IN</Link>
             </div>
         </div>
         <Outlet/>
