ef42273d55f0410db288f7bdc02b5443881a970c
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-16 15:22:07 +0000

    Adding the signupForm component

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,47 +1,58 @@
-import { Fragment } from "react"
+import { Fragment , useState } from "react"
 
+const defaultFormFeilds = {
+  displayName : "",
+  email       : "",
+  password    : "",
+  confirmPassword : "",
+}
 
 const SignupForm = () => {
+    const [formInputs,setFormInputs] = useState(defaultFormFeilds)
+    const {displayName,email,password,confirmPassword} = formInputs
+   
+    console.log(formInputs)
+
+    const formInputChange = (event) => {
+        const {name,value} = event.target
+        setFormInputs({...formInputs,[name]:value})
+    }
     return(
         <Fragment>
-          <form>
-           <label>Display Name</label>
-           <input 
-           type = "text" 
-           className="" 
-           onChange={} 
-           value={} 
-           required
-           />
-           
-           <label>Email</label>
-           <input 
-           type = "email" 
-           className="" 
-           onChange={} 
-           value={} 
-           required
-           />
-           
-           <label>Password</label>
-           <input 
-           type = "password" 
-           className="" 
-           onChange={} 
-           value={} 
-           required
-           />
-           
-           <label>Confirm Password</label>
-           <input 
-           type = "password" 
-           className="" 
-           onChange={} 
-           value={} 
-           required
-           />
+          <form onSubmit={() => {}}>
+            <label>Display Name</label>
+            <input 
+            type = "text" 
+            onChange={formInputChange} 
+            value={displayName}
+             name="displayName" 
+             required/>
+            
+            <label>Email</label>
+            <input 
+            type = "email" 
+            onChange={formInputChange} 
+            value={email}
+             name="email" 
+             required/>
+            
+            <label>Password</label>
+            <input 
+            type = "password" 
+            onChange={formInputChange} 
+            value={password}
+             name="password" 
+             required/>
+            
+            <label>Confirm Password</label>
+            <input 
+            type = "password" 
+            onChange={formInputChange} 
+            value={confirmPassword}
+             name="confirmPassword" 
+             required/>
 
-
+            <button type = "submit">Submit</button>
           </form>
         </Fragment>
     )
