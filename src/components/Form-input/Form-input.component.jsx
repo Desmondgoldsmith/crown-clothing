01e6b3e3049b6631e7e71ea4eb84d5d20d5496ed
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-17 11:34:19 +0000

    Creating anew component for Form inputs

--- src/components/Form-input/Form-input.component.jsx
+++ src/components/Form-input/Form-input.component.jsx
@@ -5,7 +5,8 @@ import { Fragment } from "react"
 const FormInput = ({label,...otherProps}) => {
     return(
         <Fragment>
-
+         <label className = "">{label}</label>
+         <input className = "" {...otherProps} />
         </Fragment>
     )
 }
