776d76f9a25be19d3dbe239be2f01ba447639fad
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-17 11:45:57 +0000

    Creating anew component for Form inputs

--- src/components/Form-input/Form-input.component.jsx
+++ src/components/Form-input/Form-input.component.jsx
@@ -1,13 +1,17 @@
-import { Fragment } from "react"
 
 
 
 const FormInput = ({label,...otherProps}) => {
     return(
-        <Fragment>
-         <label className = "">{label}</label>
-         <input className = "" {...otherProps} />
-        </Fragment>
+        <div className = "group">
+        <input className = "form-input" {...otherProps} />
+         {label && (
+            <label className={`${
+            otherProps.value.length ? 'shrink' : ''
+            } form-input-label`}>{label}
+          </label>
+        )}
+        </div>
     )
 }
 
