56998d7d731b3ed7b4d3abb79ec580e47088a21f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-18 22:52:42 +0000

    Adding context API

--- src/Context/User-context.jsx
+++ src/Context/User-context.jsx
@@ -1,11 +1,12 @@
 import {createContext,useState} from 'react'
 
 export const userContext = createContext({
-
+ currentUser : null,
+ setCurrentUser: () => null
 })
 
 export const userProvider = ({children}) => {
     const [currentUser,setCurrentUser] = useState(null)
     const value = {currentUser,setCurrentUser}
-    return 
+    return <userProvider.Provier value = {value}>{childern}</userProvider.Provier>
 }
