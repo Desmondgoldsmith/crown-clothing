b2e2493670ef82be443682b0f7477b66a2633a8e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-27 14:09:28 +0000

    first page implimented

--- src/Context/User-context.jsx
+++ src/Context/User-context.jsx
@@ -1,4 +1,5 @@
-import {createContext,useState} from 'react'
+import {createContext,useState,useEffect} from 'react'
+import {onAuthStateChangeListner} from '../Utils/Firebase/firebase.utils'
 
 export const UserContext = createContext({
  currentUser : null,
@@ -9,5 +10,13 @@ export const UserContext = createContext({
 export const UserProvider = ({children}) => {
     const [currentUser,setCurrentUser] = useState(null)
     const value = {currentUser,setCurrentUser}
+
+    useEffect(() => {
+       const unsubscribe =  onAuthStateChangeListner((user)=>{
+         console.log(user)
+       })
+       return unsubscribe
+    },[])
+
     return <UserContext.Provider value = {value}>{children}</UserContext.Provider>
 }
