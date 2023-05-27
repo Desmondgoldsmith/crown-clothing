931ef7718f92f49e4d55eabd24a30cb8120d1f5f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-27 19:42:01 +0000

    Done: added a context for products

--- src/Context/product-context.jsx
+++ src/Context/product-context.jsx
@@ -9,5 +9,4 @@ export const ProductProvider = ({children}) => {
        const [products,setProducts] = useState(PRODUCTS)
        const value = {products,setProducts}
 return <ProductContext.Provider value = {value}>{children}</ProductContext.Provider>
-
 }
--- src/Routes/Shop/Shop.component.jsx
+++ src/Routes/Shop/Shop.component.jsx
@@ -1,14 +1,15 @@
 import './shop.styles.scss'
-
-import shopData from '../../shop-data.json'
+import {useContext} from 'react'
+import {ProductContext} from '../../Context/product-context'
 
 const Shop = () => {
+  const {products} = useContext(ProductContext)
   return(
         <div>
-            {shopData.map(({name,id}) => {
+            {products.map(({name,id}) => {
                 return(
                     <div key = {id}>
-                    <h1>{name}</h1>
+                        <h1>{name}</h1>
                     </div>
                 )
             })}
