8ff569ef598a01cfc4845cabe0226efcf1c8d3f0
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-27 19:47:59 +0000

    Done: added a context for products

--- src/components/Card/card.component.jsx
+++ src/components/Card/card.component.jsx
@@ -1,9 +1,15 @@
 import './card.styles.scss'
+import Button from '../Button/Button.component'
 
 const Card = () => {
   return(
-    <div>
-        
+    <div className = "product-card-container">
+        <img/>
+        <div className = "footer">
+           <span className = "name"></span>
+           <span className = "price"></span>
+        </div>
+    <Button buttonType = "inverted">add to cart</Button>
     </div>
   )
 }
