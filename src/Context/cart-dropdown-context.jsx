21ce78d1ca2c22cb39a797ecc81150d28a7b3d9b
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-31 18:14:36 +0000

    Adding a context to toggle cart dropdown

--- src/Context/cart-dropdown-context.jsx
+++ src/Context/cart-dropdown-context.jsx
@@ -1,13 +1,33 @@
 import {createContext, useState} from 'react'
 
 
+const addCartItem = ({cartItems, productToAdd}) => {
+    const existingItems = cartItems.find((cartItem) => cartItem.id === productToAdd.id)
+    
+    if(existingItems){
+       cartItems.map((cartItems) => cartItems.id === productToAdd.id 
+       ? {...cartItems, quantity : cartItems.quantity + 1} 
+       : cartItems 
+       )
+    } 
+    return [...cartItems,{...productToAdd, quantity: 1}]
+}
+
 export const CartContext = createContext({
  isOpen : false,
- isSetOpen : () => {}
+ isSetOpen : () => {},
+ cartItems : [],
+ addItemsToCart : () => {}
 })
 
 export const CartProvider = ({children}) => {
        const [isOpen,isSetOpen] = useState(false)
-       const value  = {isOpen,isSetOpen}
+       const [cartItems,setCartItems] = useState([])
+
+      const addItemsToCart = ({productToAdd}) => {
+      return addCartItem(setCartItems(cartItems, productToAdd))
+      }
+
+     const value  = {isOpen,isSetOpen,addItemsToCart,cartItems}
 return <CartContext.Provider value  = {value}>{children}</CartContext.Provider>
 }
