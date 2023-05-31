bd195cfc0e5458036a9903e945b9edd064fe876e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-05-31 16:02:35 +0000

    Adding a context to toggle cart dropdown

--- src/components/Cart/Cart.components.jsx
+++ src/components/Cart/Cart.components.jsx
@@ -1,9 +1,13 @@
 import './cart.styles.scss'
 import { ReactComponent as CartLogo } from '../../Asset/shopping-bag.svg'
+import {useContext} from 'react'
+import {CartContext} from '../../Context/cart-dropdown-context'
 
 const Cart = () => {
+    const {isOpen,isSetOpen} = useContext(CartContext)
+    const handelToggleCart = () => isSetOpen(!isOpen)
     return(
-        <div className = "cart-icon-container">
+        <div className = "cart-icon-container" onClick = {handelToggleCart}>
            <CartLogo className = "shopping-icon"/>
            <span className = "item-count">0</span>
         </div>
