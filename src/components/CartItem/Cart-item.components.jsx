c437122b1309c8b2f9f40b1df93d94f50eded09a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-06-01 13:09:39 +0000

    Working on the Add to Cart func with ContextAPI

--- src/components/CartItem/Cart-item.components.jsx
+++ src/components/CartItem/Cart-item.components.jsx
@@ -1,11 +1,15 @@
-
+import './cart-items.styles.scss'
 
 const CartItem = ({cartItems}) => {
-    const {name,quantity} = cartItems
+    const {name,imageUrl,price,quantity} = cartItems
   return(
-    <div>
-        <h1>{name}</h1>
-        <span>{quantity}</span>
+    <div className = "cart-item-container">
+        <img src = {imageUrl} alt = {name}/>
+        <div className = "item-details">
+            <span className = "name">{name}</span>
+            <span className = "price">{price} * Ghc{quantity}</span>
+        </div>
+        
     </div>
   )
 }
