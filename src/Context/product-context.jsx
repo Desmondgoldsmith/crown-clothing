848c0b9fbd4a2dd4c05254fcc64921677a0a7144
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-07-08 10:45:23 +0000

    Working on the cart page

--- src/Context/product-context.jsx
+++ src/Context/product-context.jsx
@@ -1,12 +1,18 @@
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
 import SHOP_DATA from '../shop-data'
+import {AddItemsToDocument} from '../Utils/Firebase/firebase.utils.jsx'
 
 export const ProductContext = createContext({
   products : []
 })
 
+
+
 export const ProductProvider = ({children}) => {
        const [products,setProducts] = useState([])
        const value = {products,setProducts}
+       useEffect(()=>{
+        AddItemsToDocument('categories', SHOP_DATA);
+      },[]);
 return <ProductContext.Provider value = {value}>{children}</ProductContext.Provider>
 }
